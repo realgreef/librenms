af1502c1f49973bcebb09a287c07277ec4b44520
Jellyfrog <Jellyfrog@users.noreply.github.com>
2020-04-11 02:11:23 +0200

    In Laravel 6, the update method of the BelongsTo relationship no longer provides mass assignment protection or fires Eloquent events. If you wish to preserve this behavior, you should call update on the model itself.

--- app/Models/PollerGroup.php
+++ app/Models/PollerGroup.php
@@ -43,7 +43,7 @@ class PollerGroup extends Model
         static::deleting(function (PollerGroup $pollergroup) {
             // handle device pollergroup fallback to default poller
             $default_poller_id = \LibreNMS\Config::get('distributed_poller_group');
-            $pollergroup->devices()->update(['poller_group' => $default_poller_id]);
+            $pollergroup->devices->update(['poller_group' => $default_poller_id]);
         });
     }
 
